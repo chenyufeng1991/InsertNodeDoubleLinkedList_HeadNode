ff14ac8db105ec7f1038b237c941d381dea29d02
chenyufeng <503434355@qq.com>
2016-03-04 13:05:33 +0800

    Initial Commit

---
+++ InsertNodeDoubleLinkedList_HeadNode/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  InsertNodeDoubleLinkedList_HeadNode
+//
+//  Created by chenyufeng on 16/3/4.
+//  Copyright © 2016年 chenyufengweb. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
